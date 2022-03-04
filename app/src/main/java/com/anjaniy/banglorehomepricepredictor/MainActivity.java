0cfaa55cab0279ba50f201218178c00e0cc1e35f
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-03-04 23:31:26 +0530

    seventh commit

--- app/src/main/java/com/anjaniy/banglorehomepricepredictor/MainActivity.java
+++ app/src/main/java/com/anjaniy/banglorehomepricepredictor/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.anjaniy.banglorehomepricepredictor.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        widgetSetup();
+    }
+
+    private void widgetSetup() {
+
     }
 }
