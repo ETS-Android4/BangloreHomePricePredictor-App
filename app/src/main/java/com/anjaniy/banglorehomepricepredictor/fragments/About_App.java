21438e234aa8af9a8c23937d6e7c8c8e8ee9605b
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-03-05 15:45:47 +0530

    twelveth commit

--- app/src/main/java/com/anjaniy/banglorehomepricepredictor/fragments/About_App.java
+++ app/src/main/java/com/anjaniy/banglorehomepricepredictor/fragments/About_App.java
@@ -12,11 +12,26 @@ import androidx.fragment.app.Fragment;
 
 import com.anjaniy.banglorehomepricepredictor.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 public class About_App extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = (inflater.inflate(R.layout.fragment_about_app,container,false));
-        return view;
+
+        Element versionElement = new Element();
+        versionElement.setTitle("Version 1.0");
+
+        return new AboutPage(getActivity())
+                .isRTL(false)
+                .setImage(R.mipmap.logo)
+                .setDescription(getString(R.string.about_app))
+                .addItem(versionElement)
+                .addEmail("anjaniy01salekar@gmail.com")
+                .addPlayStore("com.ideashower.readitlater.pro")
+                .addGitHub("Anjaniy2000/BangloreHomePricePredictor-App")
+                .create();
     }
 }
